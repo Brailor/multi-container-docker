c7fd456cf9521258d427cad39587215e1c0a8672
Viktor Ohad <ygatravaz@protonmail.com>
2019-07-09 19:32:35 +0200

    change header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Pages</Link>
         </header>
